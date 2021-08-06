1bfc2aa2b417cd351f5312f0d7d88a228b849e81
rudyn2 <c01010010@hotmail.com>
2021-08-06 21:22:50 +0000

    latest changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -20,10 +21,38 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date_string", function (req, res) {
+  let dateString = req.params.date_string;
+  console.log("Request: " + dateString)
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString)
+    res.json({
+      unix: dateInt,
+      utc: new Date(dateInt).toUTCString()
+    })
+  } else {
+    const date = new Date(dateString)
+    if (date.toString() === 'Invalid Date') {
+      res.json({
+        error: "Invalid Date"
+      })
+    }  else {
+      // if it's invalid return the answer
+      res.json({
+        unix: date.valueOf(),
+        utc: date.toUTCString()
+      });
+    }
+  }
 });
 
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({
+    utc: now.toUTCString(),
+    unix: now.getTime()
+  })
+})
 
 
 // listen for requests :)
